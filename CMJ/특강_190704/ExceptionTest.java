469e03240c4aa58d94eb64b21ca26b4c293f4416
17mirinae <mirinae@skuniv.ac.kr>
2019-07-04 14:43:14 +0900

    File Upload

---
+++ CMJ/특강_190704/ExceptionTest.java
@@ -0,0 +1,31 @@
+package CMJ.특강_190704;
+
+public class ExceptionTest {
+	public static void main(String[] args) {
+		int a = 7;
+		double b = 0;
+		
+		int[] arr = new int[5];
+		
+		/*
+			// 비정상적인 종료
+			b =	100 / a;
+			System.out.println("Some more codes before try-catch"); // 예외 발생으로 수행되지 않음
+		*/
+		
+		//정상적인 종료
+		try {
+			System.out.println(arr[5]);
+			b = 100 / a;
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("ArrayIndexOutOfBoundsException 실행");
+			System.out.println(e);
+		} catch (ArithmeticException e) {
+			System.out.println("ArithmeticException 실행");
+			a = 1;
+			b = 100 / a;
+		}
+		
+		System.out.println("Some more codes after try-catch");
+	}
+}
