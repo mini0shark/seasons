ea6e7f75985620c98573497e7a717acbc30f216f
17mirinae <mirinae@skuniv.ac.kr>
2019-07-04 16:49:05 +0900

    Similar with the professor

---
+++ CMJ/특강_190704/BankException.java
@@ -0,0 +1,7 @@
+package CMJ.Æ¯°­_190704;
+
+public class BankException extends Exception {
+	public BankException() {
+		System.out.println("Withdraw Failed! No Money! Poor Man...");
+	}
+}
---
+++ CMJ/특강_190704/BankTest.java
@@ -0,0 +1,14 @@
+package CMJ.Æ¯°­_190704;
+
+public class BankTest {
+	public static void main(String[] args) {
+		Bank bankAccount = new Bank(1, "È«±æµ¿", 10000);
+		
+		try {
+			bankAccount.deposit(4000);
+			bankAccount.withdraw(15000);
+		} catch (BankException e) {
+			System.out.println(e);
+		}
+	}
+}
