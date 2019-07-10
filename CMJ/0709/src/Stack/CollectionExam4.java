79992cfba13ba0eee86a45c4ec5ad44187842203
17mirinae <mirinae@skuniv.ac.kr>
2019-07-10 10:24:17 +0900

    LOL

--- CMJ/0709/src/Stack/CollectionExam4.java
+++
@@ -1,8 +0,0 @@
-package Stack;
-
-import java.util.Stack;
-
-public class CollectionExam4 {
-	Stack <String> stack = new Stack<>();
-	
-}
--- CMJ/0709/src/Stack/StackTest.java
+++
@@ -1,9 +0,0 @@
-package Stack;
-
-import java.util.Stack;
-
-public class StackTest {
-	public static void main(String[] args) {
-		Stack<Integer> stack = new Stack<>();
-	}
-}
