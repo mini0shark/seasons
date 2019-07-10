aa26d643a03475e4b212a84ad15ebdf44614d9f0
17mirinae <mirinae@skuniv.ac.kr>
2019-07-10 16:39:51 +0900

    File Upload

---
+++ CMJ/특강_190701/Lotto.java
@@ -0,0 +1,9 @@
+package CMJ.특강_190701;
+
+public class Lotto {
+	public static void main(String[] args) {
+		LottoMachine lottoMachine = new LottoMachine();
+		
+		lottoMachine.pickBall();
+	}
+}
---
+++ CMJ/특강_190701/LottoMachine.java
@@ -0,0 +1,17 @@
+package CMJ.특강_190701;
+
+public class LottoMachine {
+	
+	public LottoMachine() {
+		//로또 볼을 하나씩 만들어서 1-45번까지 번호를 초기화해서 로또 기계에 넣어줌
+	}
+	
+	public void mixBall() {
+		System.out.println("로또 공 섞기");
+	}
+	
+	public void pickBall() {
+		
+		System.out.println("로또 공 뽑기");
+	}
+}
